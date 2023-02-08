f53a37ddb85380b29c9743341094bdeb95972a7b
Miandritiana <miandrititana21@gmail.com>
2023-02-08 09:17:45 +0300

    exchange.php modifierna

--- application/views/exchange.php
+++ application/views/exchange.php
@@ -10,23 +10,9 @@
 
   <title>TakaloMimilo</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="..assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/linearicons.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/font-awesome.min.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/themify-icons.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/nice-select.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/nouislider.min.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/ion.rangeSlider.css" />
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/ion.rangeSlider.skinFlat.css" />
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/magnific-popup.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/main.css">
-
 </head>
-
 <body>
+<?php $this->load->view($header);?>
 <section class="owl-carousel active-product-area section_gap" style="margin-top:150px;">
 		<!-- single product slide -->
 		<div class="single-product-slider">
@@ -34,47 +20,53 @@
 				<div class="row justify-content-center">
 					<div class="col-lg-6 text-center">
 						<div class="section-title">
-							<h1>Mes objets</h1>
+							<h1>Echange</h1>
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
 								dolore
 								magna aliqua.</p>
 						</div>
 					</div>
 				</div>
-				<div class="row">
-					<!-- single product -->
-					<?php for ($i=0; $i < count($dataObjGet); $i++) { ?>
-						<div class="col-lg-3 col-md-6">
-                            <div class="single-product">
-								<img class="img-fluid" src="<?php echo base_url();?>assets/<?php echo $dataObjGet[$i]['sary']; ?>" alt="">
-								<div class="product-details">
-									<h6><?php echo $dataObjGet[$i]['nom']; ?></h6>
-									<div class="price">
-                                        <h6><?php echo $dataObjGet[$i]['description']; ?></h6>
-                                        <h6><?php echo $dataObjGet[$i]['prixEstimatif']; ?> $</h6>
-									</div>
+					<!-- Start category Area -->
+	<section class="category-area">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-8 col-md-12">
+					<div class="row">
+						<?php for ($i=0; $i < count($data); $i++) { ?>
+							<div class="col-lg-4 col-md-4">
+								<div class="single-deal">
+									<div class="overlay"></div>
+									<img class="img-fluid w-100" src="<?php echo base_url();?>assets/<?php echo $data[$i]['sary']; ?>" width="500px">
+									<a href="<?php echo base_url('TakaloAdmin/change');?>?idObjGet=<?php echo $data[$i]['id']; ?>" class="img-pop-up" target="_blank">
+										<div class="deal-details">
+											<h6 class="deal-title">Anah ho atakalo</h6>
+										</div>
+									</a>
 								</div>
 							</div>
-							<?php } ?>
-							<?php for ($i=0; $i < count($data); $i++) { ?>
-							<div class="single-product">
-								<img class="img-fluid" src="<?php echo base_url();?>assets/<?php echo $data[$i]['sary']; ?>" alt="">
-								<div class="product-details">
-									<h6><?php echo $data[$i]['nom']; ?></h6>
-									<div class="price">
-                                        <h6><?php echo $data[$i]['description']; ?></h6>
-                                        <h6><?php echo $data[$i]['prixEstimatif']; ?> $</h6>
-									</div>
+						<?php } ?>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<?php for ($i=0; $i < count($dataObjSet); $i++) { ?>
+						<div class="single-deal">
+							<div class="overlay"></div>
+							<img class="img-fluid w-100" src="<?php echo base_url();?>assets/<?php echo $dataObjSet[$i]['sary']; ?>" width="10px">
+							<a href="#" class="img-pop-up" target="_blank">
+								<div class="deal-details">
+									<h6 class="deal-title"><?php echo $dataObjSet[$i]['nom']; ?></h6>
+									<h6 class="deal-title">Atakaloko</h6>
 								</div>
-							</div>
+							</a>
 						</div>
 					<?php } ?>
-					<!-- single product -->
 				</div>
 			</div>
 		</div>
-		<!-- single product slide -->
-</section>
-</body>
+	</section>
+	<!-- End category Area -->
+	<?php $this->load->view($footer);?>
 
+</body>
 </html>
